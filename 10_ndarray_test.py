13801c9d13287bc4edb3c6b2815cb6ad2387d11f
persocom01 <persocom02@gmail.com>
2019-10-07 15:53:34 +0800

    0.02

---
+++ 10_ndarray_test.py
@@ -0,0 +1,11 @@
+# The most important object in NumPy is the ndarray.
+# It is similar to a python list but comes with more functions and methods.
+import numpy as np
+
+grid = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+])
+
+print(type(grid))
