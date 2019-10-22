cf6af2cfd805811ba77eb745fb783725bc4afde9
persocom01 <persocom02@gmail.com>
2019-10-22 14:44:40 +0800

    0.13

--- numpy/08_np_statistics_test.py
+++ numpy/08_np_statistics_test.py
@@ -14,3 +14,15 @@ print('max: ', np.amax(a, 1))
 # as I can tell. By default it flattens the array.
 # ptp returns the size of the range (max - min).
 print('ptp row: ', np.ptp(a, axis=1))
+
+# np.percentile(arr, q, axis).
+print('percentile: ', np.percentile(a, 70))
+# np.median(arr, axis).
+print('median: ', np.median(a))
+# np.mean(arr, axis).
+print('mean: ', np.mean(a, axis=1))
+# np.average(arr, weights) uses a weighted average instead of regular mean.
+weights = [1, 2, 4]
+print('average: ', np.average(a, axis=1, weights=weights))
+print('standard deviation: ', np.std(a, axis=1))
+print('variance: ', np.var(a, axis=1))
