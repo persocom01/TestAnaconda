2117241168e0f270b3f61d9040af2934cad875d9
persocom01 <persocom02@gmail.com>
2019-10-22 11:21:29 +0800

    0.13

--- numpy/08_np_statistics_test.py
+++ numpy/08_np_statistics_test.py
@@ -9,3 +9,8 @@ a = np.array([
 # np.amin(arr, dimension).
 print('min: ', np.amin(a, 1))
 print('max: ', np.amax(a, 1))
+
+# np.ptp(arr, axis). axis=1 = horizontal, axis=0 = vertical as far
+# as I can tell. By default it flattens the array.
+# ptp returns the size of the range (max - min).
+print('ptp row: ', np.ptp(a, axis=1))
