93e44edc79e0bd48ad17f45d68e2156e7520f3dd
persocom01 <persocom02@gmail.com>
2019-10-25 15:42:15 +0800

    0.19

--- pandas/22_pd_load_file_test.py
+++ pandas/22_pd_load_file_test.py
@@ -1,2 +1,12 @@
 # Demonstrates how to load data from files in pandas.
 import pandas as pd
+
+# If you wish to open a file dialog option instead, use:
+# filedialog.askopenfilename()
+import_path = r'.\pandas\SacramentocrimeJanuary2006.csv'
+# pandas accepts other datatypes, including:
+# pd.read_excel('xls or xlsx')
+# pd.read_table('xml')
+# pd.read_json('json')
+data = pd.read_csv(import_path)
+print(data)
