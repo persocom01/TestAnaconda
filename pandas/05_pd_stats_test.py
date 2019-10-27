0bd713122996e71ef6212c0c2d1cac950044fbaa
persocom01 <persocom02@gmail.com>
2019-10-27 22:30:14 +0800

    0.24

--- pandas/05_pd_stats_test.py
+++ pandas/05_pd_stats_test.py
@@ -1,10 +1,55 @@
 # Demonstrates the stat functions included in pandas.
+import numpy as np
 import pandas as pd
 
-data = {'col1': [2, 1, 1, 1], 'col2': [1, 3, 2, 4], 'col3': [1, 2, 3, 1]}
+# np.NaN is used to represent an empty cell.
+# nan is treated as non existant and not 0.
+data = {'col1': [-2, 1, 1, 1], 'col2': [1, 3, np.NaN, 4], 'col3': [1, 2, 3, 1]}
 df = pd.DataFrame(data)
+
+# Returns number of non nan cells per column.
+print('count:')
+print(df.count())
+print()
+print('sum:')
+print(df.sum())
+print()
+print('mean:')
+print(df.mean())
+print()
+print('median:')
+print(df.median())
+print()
+# Will return multiple nodes if all of them occur the same number of
+# times.
+print('mode:')
+print(df.mode())
+print()
+print('std deviation:')
+print(df.std())
+print()
+print('min:')
+print(df.min())
+print()
+print('max:')
+print(df.max())
+print()
+print('abs:')
+print(df.abs())
+print()
+print('product:')
+print(df.prod())
+print()
+print('cumulative sum:')
+print(df.cumsum())
+print()
+print('cumulative product:')
+print(df.cumprod())
+print()
 
 # Gives a number of DataFrame stats, being size, mean, std deviation,
 # min, 25th, 50th, 75th percentile and max.
 # Not the same as scipy describe().
+print('describe:')
 print(df.describe())
+print()
