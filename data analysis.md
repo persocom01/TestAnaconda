bd611bc819e7314b5bc78da07f20d95ab710173e
persocom01 <clarencett@hotmail.com>
2019-11-04 12:05:22 +0800

    0.34

--- data analysis.md
+++ data analysis.md
@@ -3,3 +3,9 @@
 3. Check the data types of the columns using .dtypes() and adjust as necessary.
 4. Remove any columns you're not interested in including and make sure the names of remaining columns are clear.
 5. Get the summary statistics of the data using .describe().
+
+Standard error
+The standard error = standard deviation / sqrt (sample size)
+It describes the dispersion of the sample statistic (such as mean)
+around the population mean, giving you an idea whether more samples
+are needed or not.
--- pandas/01_pd_series_test.py
+++ pandas/01_pd_series_test.py
@@ -5,15 +5,17 @@ import pandas as pd
 
 # Also works with a single key dictionary, but the column label causes problems
 # with some methods such as s.unique()
-data = ['apple', 'banana', 'orange', 'apple', 'pear', 'orange']
+data = ['apple', 'banana', 'orange', 'apple', 'orange']
 s = pd.Series(data)
 print(s)
+print()
 
-# s.map(self, func_dict_series, na_action=None) returns the series with its
+# s.map(self, func_dict_series) returns the series with its
 # values maped using the argument passed.
+print('map:')
 print(s.map({
     'apple': 'pie',
     'banana': 'cake',
     'orange': 'juice',
-    'pear': 'candy'
 }))
+print()
