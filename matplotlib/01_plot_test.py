4d88299e855432603c4d26676e0d4b9e910bb348
persocom01 <clarencett@hotmail.com>
2019-11-04 10:49:51 +0800

    0.34

--- matplotlib/01_plot_test.py
+++ matplotlib/01_plot_test.py
@@ -7,6 +7,13 @@ y = np.sin(x**2)
 # Matplotlib objects are composed of two parts, the figure and the axes.
 # They can be created together using plt.subplots(figsize=(int, int)).
 fig, ax = plt.subplots(figsize=(10, 10))
-ax.plot(x, y)
+
+# ax.plot(x, y, fmt, data, scalex=True, scaley=True, **other_line2d_properties)
+# x is optional, y is the y-axis, fmt is a special format string that comprises
+# [marker][line][color]. The syntax, and the arguments for
+# **other_line2d_properties can be found here:
+# https://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.plot
+# scalex determines if view limits are adapted to the data.
+ax.plot(x, y, 'o-b')
 
 print(plt.gca())
--- pandas/01_pd_series_test.py
+++ pandas/01_pd_series_test.py
@@ -3,8 +3,17 @@
 # Series.
 import pandas as pd
 
-# Also works with plain lists, but results in no column label.
-# Some methods may not work well with column labels, such as s.unique()
-data = {'fruits': ('apple', 'banana', 'orange', 'apple', 'pear', 'orange')}
+# Also works with a single key dictionary, but the column label causes problems
+# with some methods such as s.unique()
+data = ['apple', 'banana', 'orange', 'apple', 'pear', 'orange']
 s = pd.Series(data)
 print(s)
+
+# s.map(self, func_dict_series, na_action=None) returns the series with its
+# values maped using the argument passed.
+print(s.map({
+    'apple': 'pie',
+    'banana': 'cake',
+    'orange': 'juice',
+    'pear': 'candy'
+}))
