d18f1d4a52a2bb6565434bc0daf9d54fc7e382a8
persocom01 <clarencett@hotmail.com>
2019-11-05 11:07:47 +0800

    0.35

--- numpy/01a_np_ndarray_test2.py
+++ numpy/01a_np_ndarray_test2.py
@@ -4,19 +4,22 @@ import numpy as np
 # np.empty(shape, dtype=float, order='C') creates an ndarray with random values
 # of the specified shape and dtype,
 arr = np.empty([2, 3], dtype=float)
-print('empty:\n', arr)
+print('empty:')
+print(arr)
 print()
 
 # np.zeros(), which does the same thing but filled with zeros instead.
 arr = np.zeros([2, 3], dtype=float)
-print('zeros:\n', arr)
+print('zeros:')
+print(arr)
 print()
 
 # np.arange(start, end, step, dtype) creates evenly spaced values within a
 # given range. start defaults to zero if only a single number is given as an
 # argument. The range excludes the end, just like in python range().
 arr = np.arange(0, 15, 3, dtype=int)
-print('arange:\n', arr)
+print('arange:')
+print(arr)
 print()
 
 # np.linspace(start, end, steps, endpoint=True, retstep=False, dtype)
@@ -24,14 +27,16 @@ print()
 # last step is always stop, unlike range().
 # retstep gives you the interval value.
 arr, interval = np.linspace(0, 100, 5, retstep=True, dtype=int)
-print('linspace:\n', arr)
+print('linspace:')
+print(arr)
 print('interval: ', interval)
 print()
 
 # np.logspace(start, end, num=50, endpoint=True, base=10, dtype)
 # is like np.linspace, but the range is given in powers of base 10 by default.
 arr = np.logspace(1, 10, num=10, base=2)
-print('logspace:\n', arr)
+print('logspace:')
+print(arr)
 print()
 
 # Demonstrates the np.random module to generate ndarrays.
@@ -40,20 +45,23 @@ np.random.seed(123)
 # np.random.rand(*size) generates numbers between 0 and 1.
 # *size accepts any number of arguments as a new size dimension.
 arr = np.random.rand(2, 3)
-print('rand:\n', arr)
+print('rand:')
+print(arr)
 print()
 # np.random.randint(start, stop, size) generates random integers between
 # start and stop-1.
 r = np.random.randint(0, 10)
-print('rand int: ', r)
+print('rand int:', r)
 print()
 # Arrays with more dimensions may be generated using a longer tuple for size.
 arr = np.random.randint(0, 10, size=(3, 3))
-print('rand int 2d array:\n', arr)
+print('rand int 2d array:')
+print(arr)
 print()
 # np.random.randn(*size) returns samples from a normal distribution.
 arr = np.random.randn(2, 3)
-print('randn:\n', arr)
+print('randn:')
+print(arr)
 print()
 # Use np.random.normal if you don't just want a default (mean=0, std=1)
 # normal distribution.
@@ -61,10 +69,19 @@ mean = 10
 std = 2
 size = (2, 3)
 arr = np.random.normal(mean, std, size=size)
-print('normal\n', arr)
+print('normal:')
+print(arr)
 print()
 # np.random.permutation(int or arr) returns a randomly arranged list of numbers
 # range(int) if int is given, or a randomly arranged list if list is given.
 arr = np.random.permutation(10)
-print('permutation\n', arr)
+print('permutation:')
+print(arr)
+print()
+
+# Demonstrate adding a new axis to a 1d array.
+print('new axis:')
+print(arr.shape)
+arr = arr[np.newaxis, :]
+print(arr.shape, arr)
 print()
