5450245ea75ae9816ee372ea42b928f67d89d0cb
persocom01 <clarencett@hotmail.com>
2019-11-06 17:49:33 +0800

    0.43

--- pandas/06_pd_functions_test.py
+++ pandas/06_pd_functions_test.py
@@ -1,9 +1,1 @@
 import pandas as pd
-
-data = {
-    'col1': [2, 1, 1, 1],
-    'col2': [1, 3, 2, 4],
-    'col3': [1, 2, 3, 1]
-}
-df = pd.DataFrame(data)
-print(df.pct_change())
---
+++ pandas/12_pd_stat_functions_test.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# data = {
+#     'col1': [3, 5, 1, 1, 4],
+#     'col2': [1, 3, 2, 4, 2],
+#     'col3': [1, 2, 3, 1, 1]
+# }
+# df = pd.DataFrame(data)
+
+import_path = r'.\datasets\drinks.csv'
+# Necessary in this case since 'NA'='North America' in this dataset.
+data = pd.read_csv(import_path, na_filter=False)
+df = pd.DataFrame(data[['beer_servings', 'spirit_servings', 'wine_servings']])
+print(df.head())
+print()
+
+# pct_change(self, periods=1, fill_method='pad', limit=None, freq=None, **kwargs)
+# Doesn't actually return a percentage, but the number of times the previous
+# value had to be multiplied by to get the current value: (after - prev) / prev
+# Also why the first row is always nan.
+# I haven't figured out what periods actually does. Don't use it for now.
+print(df.head().pct_change())
+print()
+
+# df.cov(self, min_periods=None)
+# The covariance is the non-normalized correlation between two variables.
+# It's probably not as useful as the correlation.
+print(df.cov())
+print()
+
+# df.corr(self, method='pearson', min_periods=1)
+# Returns a correlation matrix, which is most useful as an input of
+# sns.heatmap()
+# method accepts 3 possible string arguments:
+# 'pearson': evaluates the linear relationship between two continuous variables.
+# 'kendall': evaluates if two variables are ordered in the same way. You will
+# probably seldom use this.
+# 'spearman': evaluates the monotonic relationship between two continuous or
+# ordinal variables. In other words, when evaluating things like a ranking
+# where the difference between ranks doesn't necessarily imply that they are
+# close together.
+sns.heatmap(df.corr())
+plt.show()
