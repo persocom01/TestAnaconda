d4249882f691242907021d8a4538add9c5013e43
persocom01 <clarencett@hotmail.com>
2019-11-07 17:11:11 +0800

    0.44

--- pandas/08_pd_iteration_test.py
+++ pandas/08_pd_iteration_test.py
@@ -2,13 +2,33 @@
 import pandas as pd
 
 characters = {
-    'name': ['Kazuma', 'Aqua', 'Megumin', 'Darkness', 'Chris', 'Yunyun', 'Wiz', 'Kyouya'],
-    'class': ['adventurer', 'priest', 'crusader', 'wizard', 'thief', 'wizard', 'wizard', 'swordmaster'],
+    'name': ['Kazuma', 'Aqua', 'Megumin', 'Darkness', 'Chris', 'Yunyun', 'Wiz'],
+    'class': ['adventurer', 'priest', 'crusader', 'wizard', 'thief', 'wizard', 'wizard'],
 }
 df = pd.DataFrame(characters)
 
-# Iteritems
+# df.iteritems is basically the pandas equivalent of python dict.items().
+# It returns the column names and a list of values for each column.
 print('iteritems:')
 for k, v in df.iteritems():
     print(k)
     print(v)
+print()
+
+# df.iterrows returns the index and a dictionary of values for each row, the
+# dictionary keys being column names.
+print('iterrows:')
+for i, v in df.iterrows():
+    if i in range(3):
+        print(i)
+        print(v['name'])
+print()
+
+# df.itertuples(self, index=True, name='Pandas') is like iterrows but it
+# returns a single named tuple object. Like regular tuples, only integers
+# to slice the object.
+print('itertuples:')
+for t in df.itertuples():
+    if t[0] in range(3):
+        print(t)
+print()
