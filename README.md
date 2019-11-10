7439e4b48403fb1b0af8bb103da25d076fd480d8
persocom01 <persocom02@gmail.com>
2019-11-10 13:22:35 +0800

    0.49

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ An Anaconda testing playground based on tutorials found here:
 * scipy https://www.tutorialspoint.com/scipy/index.htm
 * pandas https://www.tutorialspoint.com/python_pandas/index.htm
 * matplotlib https://matplotlib.org/3.1.1/tutorials/index.html
+* seaborn https://www.tutorialspoint.com/seaborn/index.htm
 
 Workable code is written on each topic to demonstrate use of the python module in the topic area.
 
--- matplotlib/03_scatter_test.py
+++ matplotlib/03_scatter_test.py
@@ -11,3 +11,25 @@ plt.scatter('a', 'b', c='c', s='d', data=data)
 plt.xlabel('entry a')
 plt.ylabel('entry b')
 plt.show()
+plt.clf()
+
+
+# Demonstrates a function to plot multiple graphs at a time.
+def subplot_scatter(df, xcols, ycols, titles=None, xlabels=None, ylabels=None):
+    nrows = int(np.ceil(len(xcols)/2))
+    fig, ax = plt.subplots(nrows=nrows, ncols=2, figsize=(16, 5*nrows))
+    ax = ax.ravel()
+    for i, col in enumerate(xcols):
+        ax[i].scatter(df[col], df[ycols[i]], [1, 2, 3, 4])
+        if titles:
+            ax[i].set_title(titles[i])
+        if xlabels:
+            ax[i].set_xlabel(xlabels[i])
+        if ylabels:
+            ax[i].set_ylabel(ylabels[i])
+        # Draws a diagonal line across the graph.
+        ax[i].plot([0, 1], [0, 1], transform=ax[i].transAxes,
+                   color='red', linestyle='dashed', linewidth=1)
+
+
+subplot_scatter(data, ['a', 'a', 'a'], ['b', 'c', 'd'])
