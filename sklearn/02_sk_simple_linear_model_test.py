fa4d4738b18201845531784a43bb2d1b60241bd8
persocom01 <persocom02@gmail.com>
2019-11-17 23:49:43 +0800

    0.65

--- sklearn/02_sk_simple_linear_model_test.py
+++ sklearn/02_sk_simple_linear_model_test.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import sklearn.datasets as skds
 import sklearn.model_selection as skms
 import sklearn.linear_model as sklm
@@ -6,6 +8,7 @@ import sklearn.metrics as skm
 
 data = skds.load_diabetes()
 df = pd.DataFrame(data.data, columns=data.feature_names)
+s = pd.Series(data.target, )
 print(df.head())
 print()
 
@@ -13,8 +16,11 @@ print()
 # you can get a list of feature names with the following code, before passing
 # it to the DataFrame as a list of columns:
 # features = [col for col in df.columns if col != 'target']
-X = df
-y = data.target
+# We use [:, np.newaxis] in this case to add a y axis to the output to make it
+# a 2d array so it can be accepted by the sklm.LinearRegression() later.
+X = df['bmi'][:, np.newaxis]
+# Convert to ndarray to avoid problems later.
+y = np.array(data.target)
 
 # skms.train_test_split(arr_features, arr_target, test_size=0.25, **options)
 # options accepts a number of arguments, including:
@@ -30,13 +36,15 @@ y = data.target
 # In this case, the 'age' column was effectively made a categorical column
 # where the proportion of 'age' below 0 is the same in the training and
 # testing split.
-X_train, X_test, y_train, y_test = skms.train_test_split(X, y, stratify=(df['age'] < 0))
+X_train, X_test, y_train, y_test = skms.train_test_split(
+    X, y, stratify=(df['age'] < 0))
 print('train:')
-print(X_train.head())
+print(X_train[:5])
 print('count:', len(X_train))
 print()
 print('test:')
-print(X_test.head())
+print(X_test[:5])
+print('count:', len(X_test))
 print()
 
 # Demonstrates training a model on test data.
@@ -51,20 +59,51 @@ lm = sklm.LinearRegression().fit(X_train, y_train)
 # lm.predict(self, X) returns y values predicted by the model for input values
 # of X. We normally pass it the X_test values because we want to see how close
 # predicted y is to y_test.
-lm_predict = lm.predict(X_test)
+y_hat = lm.predict(X_test)
 print('predictions:')
-print(lm_predict[:10])
-print('count:', len(lm_predict))
+print(y_hat[:10])
+print('count:', len(y_hat))
 print()
 
+print('coefficients:', lm.coef_)
+print()
+
+print('intercept:', lm.intercept_)
+print()
+
+# Model performance measures.
+print('mean absolute error(MAE):', round(
+    skm.mean_absolute_error(y_test, y_hat), 2))
+print('mean squared error(MSE):', round(
+    skm.mean_squared_error(y_test, y_hat), 2))
+print('median absolute error:', round(
+    skm.median_absolute_error(y_test, y_hat), 2))
+# skm.explained_variance_score(y_true, y_pred, sample_weight=None,
+# multioutput=’uniform_average’)
+# explained_variance_score is in effect an unbiased version of R**2. R**2 will
+# return the same result regardless of the bias of the model, reflecting only
+# the closeness of the slope of the prediction with the slope test values,
+# regardless of the y intercept (bias).
+print('explained variance score:', round(
+    skm.explained_variance_score(y_test, y_hat), 2))
 # skm.r2_score(y_true, y_pred, sample_weight=None,
 # multioutput=’uniform_average) returns the R**2 value of the prediction,
 # where:
 # R**2 = 1 - (sum of squared residuals) / (total sum of squares)
-# residuals being (prediction - mean)**2,
+# squared residuals being (actual value - prediction)**2,
 # and squares being (actual value - mean)**2.
 # The maximum value of R**2 is 1.0, but can be negative, if the model performs
 # worse than the mean.
-print('R^2:')
-print(skm.r2_score(y_test, lm_predict))
+print('R**2:', round(skm.r2_score(y_test, y_hat), 2))
 print()
+
+# Plot the graph.
+fig, ax = plt.subplots(figsize=(12, 7.5))
+ax.plot([], [], ' ', label=r'$R^2 = $' +
+        f'{round(skm.r2_score(y_test, y_hat), 2)}')
+ax.scatter(X_test, y_test, alpha=0.7, label='test values')
+ax.plot(X_test, y_hat, color='g', alpha=0.7, label='linear regression line')
+ax.legend()
+
+for i, y in enumerate(y_hat.ravel()):
+    ax.plot([X_test[i], X_test[i]], [y, y_test[i]], color='r', alpha=0.7)
