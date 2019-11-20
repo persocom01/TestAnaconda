0ab660e4641dedec66f4958969ab1999577decec
persocom01 <clarencett@hotmail.com>
2019-11-20 11:39:12 +0800

    0.70

--- sklearn/02_sk_preprocessing_test.py
+++ sklearn/02_sk_preprocessing_test.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pandas as pd
+import sklearn.model_selection as skms
 import sklearn.preprocessing as skpp
 import sklearn.compose as skc
 
@@ -13,8 +13,13 @@ df = pd.DataFrame(data)
 # Gets rid of any string variables.
 df = pd.get_dummies(df, columns=['continent'], prefix='cont', drop_first=True)
 features = [col for col in df.columns if col != 'total_litres_of_pure_alcohol']
-target = df['total_litres_of_pure_alcohol']
+target = 'total_litres_of_pure_alcohol'
 print(df.head())
+
+X = df[features]
+y = df[target]
+
+X_train, X_test, y_train, y_test = skms.train_test_split(X, y)
 
 # skc.ColumnTransformer(transformers, remainder=’drop’, sparse_threshold=0.3,
 # n_jobs=None, transformer_weights=None, verbose=False)
@@ -27,21 +32,23 @@ ct = skc.ColumnTransformer(
     # range equal to the feature range.
     # copy=True means the new object will not replace the old.
     # Trying to scale a categorical variable will result in a ValueError.
-    [('mm_scaler', skpp.MinMaxScaler(feature_range=(0, 10)), ['beer_servings']),
+    [('mms', skpp.MinMaxScaler(feature_range=(0, 10)), ['beer_servings']),
      # skpp.StandardScaler(copy=True, with_mean=True, with_std=True) scales
      # variables on a scale of +- std deviations about the mean.
-     ('std_scaler', skpp.StandardScaler(), ['spirit_servings'])],
+     ('ss', skpp.StandardScaler(), ['spirit_servings'])],
     remainder='passthrough')
 # If a warning occurs, set:
-# pd.options.mode.chained_assignment = None
-df[features] = ct.fit_transform(df[features])
+pd.options.mode.chained_assignment = None
+X_train[features] = ct.fit_transform(X_train[features])
+# The test set will be transformed but not fitted.
+X_test[features] = ct.transform(X_test[features])
 print('MinMaxScaler beer and StandardScaler spirits:')
-print(df.head())
+print(X_train.head())
 
 # skpp.Normalizer(norm=’l2’, copy=True) scales the variables such that the sum
 # of all squares in the row=1. I'm not sure what this is used for.
-normalizer = skpp.Normalizer()
-df2 = pd.DataFrame(normalizer.fit_transform(df), columns=df.columns)
+norm = skpp.Normalizer()
+X_train[features] = norm.fit_transform(X_train[features])
 print('normalizer:')
-print(df2[:3].values)
+print(X_train.values[:3])
 print()
