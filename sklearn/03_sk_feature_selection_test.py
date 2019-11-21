7345d828e8adccf2ad5e295f20515fd7c029aebe
persocom01 <clarencett@hotmail.com>
2019-11-21 15:14:55 +0800

    0.73

--- sklearn/03_sk_feature_selection_test.py
+++ sklearn/03_sk_feature_selection_test.py
@@ -21,7 +21,21 @@ y = data.target
 
 X_train, X_test, y_train, y_test = skms.train_test_split(X, y, random_state=1, stratify=data.target)
 
-np.set_printoptions(precision=2)
-skb = skfs.SelectKBest(score_func=skfs.chi2, k=4)
+# skfs.SelectKBest(score_func=<function f_classif>, k=10)
+# k sets the ending number of desired features.
+# skfs.chi2(X, y) is the chi2 test used to compare a categorical y with non
+# zero features x.
+print('chi2 selected columns:')
+# Use skb.scores_ to see the actual chi2 score.
+skb = skfs.SelectKBest(score_func=skfs.chi2, k=10)
 skb.fit(X_train, y_train)
-print(skb.scores_)
+# Preserves the column names compared to a straight skb.fit_transform()
+selected_cols = [v for i, v in enumerate(X_train.columns) if i in skb.get_support(indices=True)]
+X_train_chi2 = X_train[selected_cols]
+print(X_train_chi2.columns)
+print()
+
+# skfs.RFE(estimator, n_features_to_select=None, step=1, verbose=0) performs
+# recursive feature elimination
+lm = sklm.LinearRegression()
+rfe = skfs.RFE(lm, 3)
