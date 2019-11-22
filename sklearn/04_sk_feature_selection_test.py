fe3ceedb4ee39fa7ba0380db58b7e9df8dc773c7
persocom01 <clarencett@hotmail.com>
2019-11-22 12:30:30 +0800

    0.73

--- sklearn/04_sk_feature_selection_test.py
+++ sklearn/04_sk_feature_selection_test.py
@@ -42,7 +42,13 @@ y = data.target
 
 def vif_feature_select(df, max_score=5.0, n_features=-1):
     features = df.columns
-    vif_factor = [smsoi.variance_inflation_factor(df.values, i) for i in range(len(features))]
+    # smsoi.variance_inflation_factor expects a column of constants or it will
+    # get inaccurate results.
+    df = df.assign(const=1)
+    vif_factor = [smsoi.variance_inflation_factor(df.values, i) for i in range(len(features) + 1)]
+    # Delete the constant column and its vif.
+    del df['const']
+    del vif_factor[-1]
     max_vif_index = np.argmax(vif_factor)
     if n_features < 0 and vif_factor[max_vif_index] >= max_score:
         del df[features[max_vif_index]]
@@ -55,11 +61,17 @@ def vif_feature_select(df, max_score=5.0, n_features=-1):
 
 
 print('VIF:')
-X = vif_feature_select(X, 0, 10)
+X = vif_feature_select(X)
 features = X.columns
 print(features)
 print()
 
+fig, ax = plt.subplots(figsize=(12, 7.5))
+sb.heatmap(X.corr(), cmap='PiYG', annot=False)
+# Corrects the heatmap for later versions of matplotlib.
+bottom, top = ax.get_ylim()
+ax.set_ylim(bottom+0.5, top-0.5)
+# plt.show()
 
 X_train, X_test, y_train, y_test = skms.train_test_split(X, y, random_state=1, stratify=data.target)
 
@@ -67,7 +79,6 @@ X_train, X_test, y_train, y_test = skms.train_test_split(X, y, random_state=1, s
 # k sets the ending number of desired features.
 # skfs.chi2(X, y) is the chi2 test used to compare a categorical y with non
 # zero features x.
-print('chi2 feature selection:')
 # Use skb.scores_ to see the actual chi2 score.
 skb = skfs.SelectKBest(score_func=skfs.chi2, k=5)
 skb.fit(X_train, y_train)
@@ -75,6 +86,7 @@ skb.fit(X_train, y_train)
 selected_cols = [v for i, v in enumerate(X_train.columns) if i in skb.get_support(indices=True)]
 X_train_chi2 = X_train[selected_cols]
 ranking = [{v: k for k, v in enumerate(np.sort(skb.scores_)[::-1])}.get(r) for r in skb.scores_]
+print('chi2 feature selection:')
 print('feature ranking (0 being best):', ranking)
 print(X_train_chi2.columns)
 print()
@@ -83,18 +95,19 @@ print()
 # recursive feature elimination of features.
 # step determines the number of features to remove at each iteration. If step
 # is between 0.0 and 1.0, it is taken as the proportion of total features.
-print('recursive feature elimination:')
 lm = sklm.LinearRegression()
 rfe = skfs.RFE(lm, 5)
 rfe.fit(X_train, y_train)
 selected_cols = [v for i, v in enumerate(X_train.columns) if i in rfe.get_support(indices=True)]
 X_train_rfe = X_train[selected_cols]
+print('recursive feature elimination:')
 print('feature ranking (1 being best):', rfe.ranking_)
 print(X_train_rfe.columns)
 print()
 
 # skd.PCA(n_components=None, copy=True, whiten=False, svd_solver=’auto’,
 # tol=0.0, iterated_power=’auto’, random_state=None)
+# Principal component analysis.
 # n_components determines number of features to keep.
 ss = skpp.StandardScaler()
 X_train_ss = pd.DataFrame(ss.fit_transform(X_train[features]), columns=features)
@@ -103,8 +116,9 @@ pca = skd.PCA(n_components=n_comp)
 pca.fit(X_train_ss)
 most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_comp)]
 most_important_names = [features[most_important[i]] for i in range(n_comp)]
-dic = {'PC{}'.format(i): most_important_names[i] for i in range(n_comp)}
-X_train_pca = pd.DataFrame(dic.items())
+X_train_pca = X_train[most_important_names]
 # How much of the variance is explained by each feature.
+print('Principal component analysis:')
 print(pca.explained_variance_ratio_)
-print(X_train_pca)
+print(X_train_pca.columns)
+print()
