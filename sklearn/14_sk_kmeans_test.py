bdfd2588ec866cc07b53a8aab18726ed49b72d07
persocom01 <persocom02@gmail.com>
2020-01-11 00:12:12 +0800

    1.25

--- sklearn/14_sk_kmeans_test.py
+++ sklearn/14_sk_kmeans_test.py
@@ -5,6 +5,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import silhouette_score
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
