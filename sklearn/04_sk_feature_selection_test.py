e08ffbdefec7de37a872f4788f02520b0bfe836d
persocom01 <persocom02@gmail.com>
2020-02-23 07:40:20 +0800

    1.43

--- sklearn/04_sk_feature_selection_test.py
+++ sklearn/04_sk_feature_selection_test.py
@@ -26,8 +26,8 @@ print()
 
 # One of the easiest ways to eliminate features is through a heatmap.
 # A correlation of 0.2 and below is considered low. 0.75 and above is
-# considered high. Features with high correlations between themselves need to
-# be eliminated using VIF.
+# considered high. One can choose to remove features high correlations between
+# themselves based on the heatmap or use VIF instead.
 # fig, ax = plt.subplots(figsize=(12, 7.5))
 # sb.heatmap(df.corr(), cmap='PiYG', annot=False)
 # # Corrects the heatmap for later versions of matplotlib.
