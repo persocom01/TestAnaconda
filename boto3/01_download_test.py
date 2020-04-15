2fdc9ac4cd3daaf9ff3d8e9e851d01d43ea149c9
Toh <clarenceboonteck.toh@dxc.com>
2020-04-16 01:47:43 +0800

    1.55

--- boto3/01_download_test.py
+++ boto3/01_download_test.py
@@ -1,3 +1,6 @@
+# Demonstrates how to download files from an Amazon S3 server. The key will of
+# course differ for different users, and the bucket name is the server instance
+# name.
 import json
 import boto3
 import pleiades as ple
@@ -16,6 +19,8 @@ s3 = boto3.resource(
 cz = ple.CZ(s3)
 
 bucket = 'inp-dil-ap-southeast-1'
+# keys in this case are the paths to the needed files inside the bucket.
 keys = cz.get_keys(bucket, prefix='/data/eICU/')
+# Folder to save files into.
 savein = './boto3/eicu/'
 print(cz.download_files(bucket, keys, savein=savein))
