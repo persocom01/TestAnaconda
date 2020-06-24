2fe57c760cebe7c8666165e9b8bb0bc0cc58722d
Toh <clarenceboonteck.toh@dxc.com>
2020-06-24 21:10:55 +0800

    1.65

---
+++ statsmodels/04_sm_vector_autoregression_test.py
@@ -0,0 +1,51 @@
+# Vector Autoregression (VAR) is a time series model used on time series data
+# with multiple features that influence each other.
+# https://www.machinelearningplus.com/time-series/vector-autoregression-examples-python/
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import Statsmodels
+from statsmodels.tsa.api import VAR
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tools.eval_measures import rmse, aic
+
+from statsmodels.tsa.stattools import grangercausalitytests
+maxlag=12
+test = 'ssr_chi2test'
+def grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=False):
+    """Check Granger Causality of all possible combinations of the Time series.
+    The rows are the response variable, columns are predictors. The values in the table
+    are the P-Values. P-Values lesser than the significance level (0.05), implies
+    the Null Hypothesis that the coefficients of the corresponding past values is
+    zero, that is, the X does not cause Y can be rejected.
+
+    data      : pandas dataframe containing the time series variables
+    variables : list containing names of the time series variables.
+    """
+    df = pd.DataFrame(np.zeros((len(variables), len(variables))), columns=variables, index=variables)
+    for c in df.columns:
+        for r in df.index:
+            test_result = grangercausalitytests(data[[r, c]], maxlag=maxlag, verbose=False)
+            p_values = [round(test_result[i+1][0][test][1],4) for i in range(maxlag)]
+            if verbose: print(f'Y = {r}, X = {c}, P Values = {p_values}')
+            min_p_value = np.min(p_values)
+            df.loc[r, c] = min_p_value
+    df.columns = [var + '_x' for var in variables]
+    df.index = [var + '_y' for var in variables]
+    return df
+
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
+
+def cointegration_test(df, alpha=0.05):
+    """Perform Johanson's Cointegration Test and Report Summary"""
+    out = coint_johansen(df,-1,5)
+    d = {'0.90':0, '0.95':1, '0.99':2}
+    traces = out.lr1
+    cvts = out.cvt[:, d[str(1-alpha)]]
+    def adjust(val, length= 6): return str(val).ljust(length)
+
+    # Summary
+    print('Name   ::  Test Stat > C(95%)    =>   Signif  \n', '--'*20)
+    for col, trace, cvt in zip(df.columns, traces, cvts):
+        print(adjust(col), ':: ', adjust(round(trace,2), 9), ">", adjust(cvt, 8), ' =>  ' , trace > cvt)
