9d9bbbead202d90d553f2427a4bc6ac05f660363
persocom01 <persocom02@gmail.com>
2020-06-29 16:43:19 +0800

    1.65

--- flask/start_flask.sh
+++ flask/start_flask.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # The above code is called a shebang, and it defines what program the sh script
 # is interpreted with. The most common shebangs are #!/bin/sh and #!/bin/sh
+# sh written in windows may not run in linux. To rectify this, start git bash
+# and copy paste the contents of the windows file into a vim or nano edited
+# file.
 
 export FLASK_APP=test_app
 # development mode lets the server reload itself on code changes, and also sets
