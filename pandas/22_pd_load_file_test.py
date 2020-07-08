149c5c736381e320421ba1aba69c44a21ccb28e7
persocom01 <persocom02@gmail.com>
2020-07-08 18:41:50 +0800

    1.66

--- pandas/22_pd_load_file_test.py
+++ pandas/22_pd_load_file_test.py
@@ -10,8 +10,16 @@ import time
 import_path = r'.\datasets\SacramentocrimeJanuary2006.csv'
 export_path = r'.\datasets\SacramentocrimeJanuary2006.xlsx'
 
+
+# Converting a column to a string is a useful transformation when you wish to
+# keep leading zeroes that would beb lost when converted to int, for instance
+# 007 -> 7.
+def to_string(x):
+    return str(x)
+
+
 # pd.read_csv(path, sep=',', header='infer', names=None, index_col=None,
-# usecols=None, skiprows=None, skipfooter=0, na_filter=True)
+# usecols=None, converters=None, skiprows=None, skipfooter=0, na_filter=True)
 # header=int_list determines the row to use as column label. The first row
 # corresponds to header=0.
 # names=list allows you to customize the column label. If the number of names
@@ -24,6 +32,8 @@ export_path = r'.\datasets\SacramentocrimeJanuary2006.xlsx'
 # the file instead of extracting the whole thing. Callable is a one argument
 # function that will return an int. The order of the list is not taken into
 # account.
+# converters=dict accepts a dict of {col_name: function} and runs the values
+# from each column through the function before writing it into the DataFrame.
 # skiprows=int_list_callable determines the rows to skip from the top of the
 # file, or specific rows if a list is given. Note that it will skip the column
 # labels line if you just put skiprows=1.
@@ -37,9 +47,11 @@ export_path = r'.\datasets\SacramentocrimeJanuary2006.xlsx'
 # pd.read_excel('xls or xlsx')
 # pd.read_table('xml')
 # pd.read_json('json')
-data = pd.read_csv(import_path, index_col=None, keep_default_na=False, na_values=[''])
+data = pd.read_csv(import_path, index_col=None, converters={
+                   'district': to_string}, keep_default_na=False, na_values=[''])
 df = pd.DataFrame(data)
 print(df.head())
+print(df.dtypes)
 
 # Demonstrates writing the file to excel.
 # df.to_excel(self, path, sheet_name='Sheet1')
@@ -66,11 +78,10 @@ r = requests.get(url, params=None, headers=headers)
 
 if r.status_code == 200:
     js = r.json()
+    df = pd.DataFrame(js)
+    print(df)
 else:
     print(r.status_code)
 
 # Demonstrates how to not give yourself away as a bot.
 # time.sleep(random.random()*10)
-
-df = pd.DataFrame(js)
-print(df)
