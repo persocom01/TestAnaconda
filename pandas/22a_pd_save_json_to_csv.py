e3a7d162a8962dd73310e50a62c99cda4f91d267
persocom01 <persocom02@gmail.com>
2022-11-03 15:34:10 +0800

    2.20

---
+++ pandas/22a_pd_save_json_to_csv.py
@@ -0,0 +1,19 @@
+# Demonstrates converting json files to csv using pandas
+import requests
+import pandas as pd
+
+table = 'incident'
+file_path = f'./pandas/{table}.csv'
+r = requests.get(
+    f'https://linkname/{table}',
+    auth=('user', 'password'))
+
+if r.status_code == 200:
+    data = r.json()
+    print(data)
+else:
+    print(r.json())
+
+normalized_data = pd.json_normalize(data['result'], sep='.')
+df = pd.DataFrame(normalized_data)
+df.to_csv(file_path, index=False)
